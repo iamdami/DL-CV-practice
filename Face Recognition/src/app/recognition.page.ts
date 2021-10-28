da9ca6f10921a055f05724b3f9f6c4a5fec7642d
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-10-28 20:20:10 +0900

    Add files via upload

---
+++ Face Recognition/src/app/recognition.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recognition',
+  templateUrl: './recognition.page.html',
+  styleUrls: ['./recognition.page.scss'],
+})
+export class RecognitionPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
